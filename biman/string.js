af86bd7083f449cb3f0c020a6fb9d49d1f87c710
jsb4377 <jsb4377@naver.com>
2022-11-29 17:06:26 +0900

    first.

--- biman/string.js
+++
@@ -1,7 +0,0 @@
-const msg = "Hello JavaScript";
-const splitMsg = msg.split("");
-const tel = "010-2582-2242";
-const splitTel = tel.split("-").join("");
-console.log(splitTel);
-
-//console.log(msg.charAt(msg.length - 1));
